f3de7a1ae8f7ea4a315621a26f9845602aa50858
Rohan Bari <rohanbari4@gmail.com>
2026-08-14 23:58:05 +0530

    Solved 266B
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

--- src/_2026_/_08_/P_266B.java
+++ src/_2026_/_08_/P_266B.java
@@ -1,5 +1,27 @@
 package _2026_._08_;
 
-public class P_266B {
+import java.util.Scanner;
 
+public class P_266B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // number of children
+        int t = sc.nextInt(); // number of seconds
+        String s = sc.next(); // initial queue
+        char[] queue = s.toCharArray();
+
+        for (int time = 0; time < t; time++) {
+            for (int i = 0; i < n - 1; i++) {
+                if (queue[i] == 'B' && queue[i + 1] == 'G') {
+                    queue[i] = 'G';
+                    queue[i + 1] = 'B';
+                    i++;
+                }
+            }
+        }
+
+        System.out.println(new String(queue));
+        sc.close();
+    }
 }
