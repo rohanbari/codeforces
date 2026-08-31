c41bf475c75537140f6a0ac8d05b8d875ce52635
Rohan Bari <rohanbari4@gmail.com>
2026-08-31 23:54:49 +0530

    Last problem of the August '26 month
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/_2026_/_08_/P_451A.java
@@ -0,0 +1,20 @@
+package _2026_._08_;
+
+import java.util.Scanner;
+
+public class P_451A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int min = Math.min(n, m);
+        if (min % 2 == 0) {
+            System.out.println("Malvika");
+        } else {
+            System.out.println("Akshat");
+        }
+
+        scanner.close();
+    }
+}
