5f478903d3974fd6fa7c11f4fc4f14f69c9820b9
Rohan Bari <rohanbari4@gmail.com>
2026-09-03 23:31:24 +0530

    Solved 313A
    Signed-off-by: Rohan Bari <rohanbari4@gmail.com>

---
+++ src/_2026_/_09_/P_313A.java
@@ -0,0 +1,24 @@
+package _2026_._09_;
+
+import java.util.Scanner;
+
+public class P_313A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(solution(n));
+
+        scanner.close();
+    }
+
+    private static int solution(int n) {
+        if (n >= 0) {
+            return n;
+        }
+
+        int option1 = n / 10;
+        int option2 = (n / 100) * 10 + (n % 10);
+
+        return Math.max(option1, option2);
+    }
+}
